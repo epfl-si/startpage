8b3023132b708639c6bf0a94ff3b5f4e1d586315
Nicolas Borboën <ponsfrilus@gmail.com>
2024-01-04 23:24:53 +0100

    [feature] help page

--- src/server.js
+++ src/server.js
@@ -18,6 +18,10 @@ app.get('/', function (req, res) {
 app.get('/about', function (req, res) {
   res.render('pages/about', {});
 });
+// help page
+app.get('/help', function (req, res) {
+  res.render('pages/help', {});
+});
 // catalog page
 app.get('/catalog', function (req, res) {
   const data = require(path.join(__dirname, 'data/EPFL-services.json'));
